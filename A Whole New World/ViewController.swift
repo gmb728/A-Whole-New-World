3e52c5d7d1fec4b7269dfc4aa98f9909a354ec14
Sophia <gmb728@gmail.com>
2020-02-25 15:11:36 +0800

    Initial Commit

---
+++ A Whole New World/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  A Whole New World
+//
+//  Created by Chang Sophia on 2/25/20.
+//  Copyright © 2020 Chang Sophia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
