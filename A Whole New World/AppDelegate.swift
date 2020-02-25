ac70f7fa6db7376da29f478231fcdce7ca69e786
Sophia <gmb728@gmail.com>
2020-02-25 16:59:17 +0800

    init

--- A Whole New World/AppDelegate.swift
+++ A Whole New World/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,7 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        //為了讓 App 能在背景繼續播放，在螢幕鎖定 & silent mode 都能繼續播放
+        try? AVAudioSession.sharedInstance().setCategory(.playback)
+         
         return true
     }
 
--- A Whole New World/ViewController.swift
+++ A Whole New World/ViewController.swift
@@ -7,14 +7,45 @@
 //
 
 import UIKit
+import AVFoundation
+import MediaPlayer
 
 class ViewController: UIViewController {
+    @IBOutlet weak var pauseButton: UIButton!
+     
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var lyricsLabel: UILabel!
+   
+    
+    
+    let player = AVPlayer()
+    //在class ViewController 裡宣告AVPlayer 型別的property player 生成播放音樂的AVPlayer物件
 
+   
+    
+    
+    
+    @IBAction func pauseButtonPressed(_ sender: Any) {
+         
+        player.pause()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        
+        let fileUrl = Bundle.main.url(forResource:"piano", withExtension: "mp3")!
+        //利用 Bundle.man 取得 App 主要的 Bundle，也就是 App 本身的資料夾。放在 Project navigator 下的 music.mp4 將在 App 的資料夾下，透過呼叫 function url(forResource:withExtension:) 可取得它在資料夾裡的 URL，參數 forResource 傳入檔名，參數 withExtension 傳入附檔名
+        //function url 回傳的 URL 將是 optional ，因此我們加上 ! 取值
+        let playerItem = AVPlayerItem(url: fileUrl)
+        //利用 AVPlayerItem 生成 player 要播放的音樂
+        player.replaceCurrentItem(with: playerItem)
+        //設定 player 要播放的 AVPlayerItem
+        player.play()
+        //執行
+        
+        
     }
-
-
+ 
 }
-
+ 
